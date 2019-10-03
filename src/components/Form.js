55562d70393e97d8e88d0c4a1bcf0fa34ef85f34
kundan.mahto@techjini.com <kundan.mahto@techjini.com>
2019-10-03 23:14:15 +0530

    Commit message

---
+++ src/components/Form.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity 
+} from 'react-native';
+
+export default class Logo extends Component<{}> {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+ 
+  _onPressButton() {
+    const {
+      username,
+      password,
+    } = this.state
+    alert('You tapped the button!' +username +"---"+s.password)
+  }
+
+	render(){
+		return( 
+			<View style={styles.container}>
+           <Text >Kundan Here?</Text>
+          <TextInput style={styles.inputBox} 
+              underlineColorAndroid='rgba(0,0,0,0)' 
+              placeholder="Email"
+              placeholderTextColor = "#ffffff"
+              selectionColor="#fff"
+              keyboardType="email-address"
+              onChangeText={username => this.setState({username})}
+
+
+
+              />
+          <TextInput style={styles.inputBox} 
+              underlineColorAndroid='rgba(0,0,0,0)' 
+              placeholder="Password"
+              secureTextEntry={true}
+              placeholderTextColor = "#ffffff"
+              onChangeText={password => this.setState({password})}
+
+
+              />  
+           <TouchableOpacity style={styles.button} onPress={this._onPressButton}>
+             <Text style={styles.buttonText}>{this.props.type}</Text>
+           </TouchableOpacity>     
+  		</View>
+			)
+	}
+}
+
+const styles = StyleSheet.create({
+  container : {
+    flexGrow: 1,
+    justifyContent:'center',
+    alignItems: 'center'
+  },
+
+  inputBox: {
+    width:300,
+    backgroundColor:'rgba(255, 255,255,0.2)',
+    borderRadius: 25,
+    paddingHorizontal:16,
+    fontSize:16,
+    color:'#ffffff',
+    marginVertical: 10
+  },
+  button: {
+    width:300,
+    backgroundColor:'#1c313a',
+     borderRadius: 25,
+      marginVertical: 10,
+      paddingVertical: 13
+  },
+  buttonText: {
+    fontSize:16,
+    fontWeight:'500',
+    color:'#ffffff',
+    textAlign:'center'
+  }
+  
+});
